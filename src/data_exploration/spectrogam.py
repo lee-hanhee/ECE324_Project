4492b97ae07392312824c1e7427a51002a1c0df6
Daphne490 <daphyduck490@gmail.com>
2025-01-23 15:22:06 -0500

    added data

--- src/data_exploration/spectrogam.py
+++ src/data_exploration/spectrogam.py
@@ -1,1 +1,18 @@
-print("HEllo")
+# import tensorflow as tf
+# import tensorflow_hub as hub
+import numpy as np
+import csv
+
+import matplotlib.pyplot as plt
+from IPython.display import Audio
+from scipy.io import wavfile
+
+
+wav_file_name = 'miaow_16k.wav'
+sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
+
+# Show some basic information about the audio.
+duration = len(wav_data)/sample_rate
+print(f'Sample rate: {sample_rate} Hz')
+print(f'Total duration: {duration:.2f}s')
+print(f'Size of the input: {len(wav_data)}')
