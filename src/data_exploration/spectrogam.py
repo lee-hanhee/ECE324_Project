d301d154da343b4fd77ddf2f61d81e2c66b08afd
Daphne490 <daphyduck490@gmail.com>
2025-01-26 23:24:39 -0500

    added spectrograms

--- src/data_exploration/spectrogam.py
+++ src/data_exploration/spectrogam.py
@@ -1,17 +1,34 @@
-# import tensorflow as tf
-# import tensorflow_hub as hub
 import numpy as np
 import csv
 
 import matplotlib.pyplot as plt
-from IPython.display import Audio
 from scipy.io import wavfile
+from pathlib import Path
 
-wav_file_name = 'data/raw/Track00006/mix.wav'
-sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
 
-# Show some basic information about the audio.
-duration = len(wav_data)/sample_rate
-print(f'Sample rate: {sample_rate} Hz')
-print(f'Total duration: {duration:.2f}s')
-print(f'Size of the input: {len(wav_data)}')
+def save_info(wav_data, sample_rate):
+  duration = len(wav_data)/sample_rate
+  print(f'Sample rate: {sample_rate} Hz')
+  print(f'Total duration: {duration:.2f}s')
+  print(f'Size of the input: {len(wav_data)}')
+  return wav_data, sample_rate
+
+def plot_spectrogram(wav_data, sample_rate, track_name):
+  plt.figure(figsize=(10, 6))
+  plt.specgram(wav_data, Fs=sample_rate, NFFT=1024, noverlap=512, cmap='viridis')
+  plt.title('Spectrogram of mix.wav')
+  plt.xlabel('Time (s)')
+  plt.ylabel('Frequency (Hz)')
+  plt.colorbar(label='Intensity (dB)')
+  plt.tight_layout()
+  image_path = "results/spectrograms/" + track_name + "-spectrogram.png"
+  plt.savefig(image_path)
+
+if __name__ == "__main__":
+  base_path = "data/raw"
+  for i in range(1, 21):  # Tracks are numbered from 1 to 20
+    track_name = f"Track{i:05d}"
+    wav_file = Path(base_path) / track_name  / "mix.wav"
+    sample_rate, wav_data = wavfile.read(wav_file, 'rb')
+    plot_spectrogram(wav_data, sample_rate, track_name)
+
