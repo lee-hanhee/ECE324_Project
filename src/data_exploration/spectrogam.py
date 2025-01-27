30356385d4c0175bbf9fd015a9966ad9b3376edf
Daphne490 <daphyduck490@gmail.com>
2025-01-26 23:40:56 -0500

    added boxplot/histogram

--- src/data_exploration/spectrogam.py
+++ src/data_exploration/spectrogam.py
@@ -6,6 +6,7 @@ from scipy.io import wavfile
 from pathlib import Path
 import librosa
 import librosa.display
+import seaborn as sns
 
 
 def save_info(wav_data, sample_rate):
@@ -26,8 +27,7 @@ def plot_spectrogram(wav_data, sample_rate, track_name):
   image_path = "results/spectrograms/" + track_name + "-spectrogram.png"
   plt.savefig(image_path)
 
-def plot_waveform(file_path, track_name):
-  y, sr = librosa.load(file_path)
+def plot_waveform(y, sr, track_name):
   plt.figure(figsize=(10, 4))
   librosa.display.waveshow(y, sr=sr)
   plt.title('Waveform of the Audio')
@@ -36,13 +36,36 @@ def plot_waveform(file_path, track_name):
   image_path = "results/waveforms/" + track_name + "-waveforms.png"
   plt.savefig(image_path)
 
+def calculate_rms(audio):
+    return np.sqrt(np.mean(np.square(audio)))
+
+def plot_boxplot_histogram(rms_values):
+    plt.figure(figsize=(10, 6))
+    plt.hist(rms_values, bins=20, color='skyblue', edgecolor='black')
+    plt.title('Histogram of RMS (Volume) Levels')
+    plt.xlabel('RMS (Volume Level)')
+    plt.ylabel('Frequency')
+    image_path = "results/volume_boxplot.png"
+    plt.savefig(image_path)
+
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(data=rms_values, color='skyblue')
+    plt.title('Boxplot of RMS (Volume) Levels')
+    plt.ylabel('RMS (Volume Level)')
+    image_path = "results/volume_histogram.png"
+    plt.savefig(image_path)
+
 if __name__ == "__main__":
   base_path = "data/raw"
+  rms = []
   for i in range(1, 21):  # Tracks are numbered from 1 to 20
     track_name = f"Track{i:05d}"
     wav_file = Path(base_path) / track_name  / "mix.wav"
     sample_rate, wav_data = wavfile.read(wav_file, 'rb')
+    y, sr = librosa.load(wav_file)
     # plot_spectrogram(wav_data, sample_rate, track_name)
-    # plot_waveform(wav_file, track_name)
+    # plot_waveform(y, sr, track_name)
+    rms.append(calculate_rms(y))
     print("Done " + track_name)
+  plot_boxplot_histogram(rms)
 
