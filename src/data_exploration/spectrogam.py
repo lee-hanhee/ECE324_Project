bcf1d2b887405ee36327588afb9e18b4b21b118c
Daphne490 <daphyduck490@gmail.com>
2025-03-01 14:01:32 -0500

    added an nmf pipeline

--- src/data_exploration/spectrogam.py
+++ src/data_exploration/spectrogam.py
@@ -45,15 +45,25 @@ def plot_spectrograms(mix_wav_data, instrument_wav_datas, sample_rate, track_nam
     return image_path
 
 
-def plot_waveform(y, sr, track_name):
-  plt.figure(figsize=(10, 4))
-  librosa.display.waveshow(y, sr=sr)
-  plt.title('Waveform of the Audio')
-  plt.xlabel('Time (s)')
-  plt.ylabel('Amplitude')
-  image_path = "results/waveforms/" + track_name + "-waveforms.png"
-  plt.savefig(image_path)
-  plt.close()
+def plot_waveform(mix_y, mix_sr, mix_track_name, stems_y, stems_sr):
+  n_rows = len(stems_y)+1
+  fig, ax = plt.subplots(nrows=n_rows, ncols=1, sharex=True, figsize=(10, 8))
+  for i, (y, sr) in enumerate(zip(stems_y, stems_sr)):
+    librosa.display.waveshow(y, sr=sr, ax=ax[i],label=f'Source {i}',axis='time')
+    ax[i].set(xlabel='Time [s]')
+    ax[i].legend()
+    
+  librosa.display.waveshow(mix_y, sr=mix_sr, ax=ax[-1], label="Mixed Audio", axis='time')
+  ax[-1].set(xlabel='Time [s]')
+  ax[-1].legend()
+
+  # Set the overall title
+  fig.suptitle('Waveform - ' + mix_track_name)
+
+  # Save the figure
+  image_path = f"results/waveforms/{mix_track_name}-waveforms.png"
+  fig.savefig(image_path)
+  plt.close(fig)
 
 def calculate_rms(audio):
     return np.sqrt(np.mean(np.square(audio)))
@@ -85,27 +95,33 @@ if __name__ == "__main__":
   base_path = "data/raw"
   rms = []
   instrument_data = {}
+
   for i in range(1, 21):  # Tracks are numbered from 1 to 20
     track_name = f"Track{i:05d}"
     wav_file = Path(base_path) / track_name  / "mix.wav"
-    print(wav_file)
     sample_rate, wav_mix_data = wavfile.read(wav_file, 'rb')
+    y_mix, sr_mix = librosa.load(wav_file)
+    rms.append(calculate_rms(y_mix))
+    y_stems = []
+    sr_stems = []
     for inst in range(df["Number of Instruments"][i-1]):
         stem_name = f"S{inst:02d}.wav"
         stem  = f"S{inst:02d}"
         inst_file = Path(base_path) / track_name  / Path("stems") / stem_name
-        print(inst_file)
+        # print(inst_file)
         try:
           _, wav_inst_data = wavfile.read(inst_file, 'rb')
+          y, sr = librosa.load(inst_file)
+          y_stems.append(y)
+          sr_stems.append(sr)
           instrument_data[stem] = wav_inst_data
         except:
            continue
         
 
-    plot_spectrograms(wav_mix_data, instrument_data, sample_rate, track_name)
-    y, sr = librosa.load(wav_file)
-    rms.append(calculate_rms(y))
-    plot_waveform(y, sr, track_name)
+    # plot_spectrograms(wav_mix_data, instrument_data, sample_rate, track_name)
+    
+    plot_waveform(y_mix, sr_mix, track_name, y_stems, sr_stems)
     print("Done " + track_name)
 
   plot_boxplot_histogram(rms)
